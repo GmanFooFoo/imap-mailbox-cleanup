1d8df6894e393aa5ec2c271de41a5c07d45e8e87
German Rauhut <german@rauhut.com>
2026-05-04 09:28:35 +0200

    chore: bootstrap mailbox-cleanup project (Python 3.11, click, imap-tools)

---
+++ src/mailbox_cleanup/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.1.0"
+SCHEMA_VERSION = 1  # JSON-output contract version (bump on breaking change)
---
+++ tests/test_smoke.py
@@ -0,0 +1,3 @@
+def test_import():
+    import mailbox_cleanup
+    assert mailbox_cleanup.__version__ == "0.1.0"
